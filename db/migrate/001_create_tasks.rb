df77a2c1b36740eb839402228b2d07ebac694bf2
Spencer Evans <spencevans@gmail.com>
2017-07-26 23:20:11 +0000

    rm old db files

--- db/migrate/001_create_tasks.rb
+++
@@ -1,7 +0,0 @@
-class CreateTasks < ActiveRecord::Migration[4.2]
-  def change
-    create_table :tasks do |t|
-      t.string :title
-    end
-  end
-end
--- db/migrate/003_create_comments.rb
+++
@@ -1,7 +0,0 @@
-class CreateComments < ActiveRecord::Migration[4.2]
-  def change
-    create_table :comments do |t|
-      t.string :content
-    end
-  end
-end
